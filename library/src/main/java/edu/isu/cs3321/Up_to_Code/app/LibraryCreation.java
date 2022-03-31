3ad0d2eac59ea6988865a99880cb2a4b3bce9625
sinakhajehpour <sinakhajehpour@isu.edu>
2022-03-30 22:45:42 -0600

    correct the package

--- library/src/main/java/edu/isu/cs3321/Up_to_Code/app/LibraryCreation.java
+++ library/src/main/java/edu/isu/cs3321/Up_to_Code/app/LibraryCreation.java
@@ -1,4 +1,4 @@
-package library;
+package edu.isu.cs3321.Up_to_Code.app;
 
 /**
  * This file will contain the backend for library management portion of the project
--- library/src/main/java/edu/isu/cs3321/Up_to_Code/app/ObjectiveGo.java
+++ library/src/main/java/edu/isu/cs3321/Up_to_Code/app/ObjectiveGo.java
@@ -1,4 +1,4 @@
-package library;
+package edu.isu.cs3321.Up_to_Code.app;
 
 /**
  * This file will contain the backend for the objective go game
