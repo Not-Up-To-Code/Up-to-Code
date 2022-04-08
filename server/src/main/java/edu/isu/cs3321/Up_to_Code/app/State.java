2c1760084ea078e35e75e9a7d5adf798986eabbf
ThomasEvans <Thomasrulonevans@gmail.com>
2022-04-08 16:31:41 -0600

    fixed list initialization in server card classes.

--- server/src/main/java/edu/isu/cs3321/Up_to_Code/app/State.java
+++ server/src/main/java/edu/isu/cs3321/Up_to_Code/app/State.java
@@ -1,10 +1,128 @@
 package edu.isu.cs3321.Up_to_Code.app;
 
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 public class State {
     private String name;
-    private List<CheckListItem> checklist;
+    private List<CheckListItem> checklist = new List<CheckListItem>() {
+        @Override
+        public int size() {
+            return 0;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return false;
+        }
+
+        @Override
+        public boolean contains(Object o) {
+            return false;
+        }
+
+        @Override
+        public Iterator<CheckListItem> iterator() {
+            return null;
+        }
+
+        @Override
+        public Object[] toArray() {
+            return new Object[0];
+        }
+
+        @Override
+        public <T> T[] toArray(T[] a) {
+            return null;
+        }
+
+        @Override
+        public boolean add(CheckListItem checkListItem) {
+            return false;
+        }
+
+        @Override
+        public boolean remove(Object o) {
+            return false;
+        }
+
+        @Override
+        public boolean containsAll(Collection<?> c) {
+            return false;
+        }
+
+        @Override
+        public boolean addAll(Collection<? extends CheckListItem> c) {
+            return false;
+        }
+
+        @Override
+        public boolean addAll(int index, Collection<? extends CheckListItem> c) {
+            return false;
+        }
+
+        @Override
+        public boolean removeAll(Collection<?> c) {
+            return false;
+        }
+
+        @Override
+        public boolean retainAll(Collection<?> c) {
+            return false;
+        }
+
+        @Override
+        public void clear() {
+
+        }
+
+        @Override
+        public CheckListItem get(int index) {
+            return null;
+        }
+
+        @Override
+        public CheckListItem set(int index, CheckListItem element) {
+            return null;
+        }
+
+        @Override
+        public void add(int index, CheckListItem element) {
+
+        }
+
+        @Override
+        public CheckListItem remove(int index) {
+            return null;
+        }
+
+        @Override
+        public int indexOf(Object o) {
+            return 0;
+        }
+
+        @Override
+        public int lastIndexOf(Object o) {
+            return 0;
+        }
+
+        @Override
+        public ListIterator<CheckListItem> listIterator() {
+            return null;
+        }
+
+        @Override
+        public ListIterator<CheckListItem> listIterator(int index) {
+            return null;
+        }
+
+        @Override
+        public List<CheckListItem> subList(int fromIndex, int toIndex) {
+            return null;
+        }
+    };
 
     public String getName() {
         return name;
@@ -24,5 +142,9 @@ public class State {
 
     public State(String name){
         setName(name);
+    }
+
+    public void addCheckListItem(CheckListItem item){
+        checklist.add(item);
     }
 }
