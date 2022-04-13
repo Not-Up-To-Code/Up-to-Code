31c91f1d3621bba429ecc183cd8d37db583c4d31
ThomasEvans <Thomasrulonevans@gmail.com>
2022-04-13 02:56:25 -0600

    Started implementation of card catalog.

--- client/src/main/java/edu/isu/cs3321/Up_to_Code/app/ui.java
+++ client/src/main/java/edu/isu/cs3321/Up_to_Code/app/ui.java
@@ -30,6 +30,7 @@ import javafx.collections.ObservableSet;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -44,6 +45,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.TilePane;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.SVGPath;
@@ -208,7 +210,6 @@ public class ui extends Application {
             button.setGraphic(temp);
             button.setStyle("-fx-background-color: transparent");
 
-
             ImageView tempClone = new ImageView(image);
             button.setOnAction(new EventHandler<ActionEvent>() {
                 @Override
@@ -238,16 +239,80 @@ public class ui extends Application {
         mainStage.setTitle("Essence Practice Creator");
         mainStage.setScene(scene);
     }
-    public void showCardCatalog() throws IOException {
+
+    /**
+     * Card catalog FXML changer
+     * @throws IOException
+     */
+    public void showCardCatalog() throws IOException, InterruptedException {
         FXMLLoader loader = new FXMLLoader();
         loader.setController(new uiController(this));
         loader.setLocation(getClass().getResource(cardCatalog_FXML));
         Parent root = loader.load();
         Scene scene = new Scene(root, 1000, 680);
         mainStage.setTitle("Essence Card Catalog");
-
-
         mainStage.setScene(scene);
+        loadCardCatalog();
+    }
+
+    public void loadCardCatalog() throws IOException, InterruptedException {
+        List<Alpha> alphaList = new Gson().fromJson(connect.getAlphas(), new TypeToken<List<Alpha>>(){}.getType());
+
+        for (Alpha alpha : alphaList){
+            cardFXLoader(alpha);
+        }
+
+    }
+
+    public void cardFXLoader(Alpha alpha){
+        Scene scene = mainStage.getScene();
+        TilePane tilePane = (TilePane) scene.lookup("#cardCatalogTilePane");
+
+        Button button = new Button();
+        button.setMaxWidth(300);
+        button.setMaxHeight(280);
+
+        SVGPath back = new SVGPath();
+        back.setContent("M6.615.684H319.621c3.298 0 5.953 2.655 5.953 5.953V216.023c0 3.298-2.655 5.953-5.953 5.953H6.615c-3.298 0-5.953-2.655-5.953-5.953V6.637c0-3.298 2.655-5.953 5.953-5.953z");
+        back.setFill(Paint.valueOf("black"));
+
+        SVGPath border = new SVGPath();
+        border.setContent("m6.618.001c-3.653 0-6.614 2.961-6.614 6.614V216.029c0 3.653 2.961 6.614 6.614 6.614H319.615c3.653 0 6.614-2.961 6.614-6.614V6.615c0-3.653-2.961-6.614-6.614-6.614zm0 1.322H319.615c2.943 0 5.291 2.349 5.291 5.292V216.029c0 2.943-2.348 5.292-5.291 5.292H6.618c-2.943 0-5.291-2.349-5.291-5.292V6.615c0-2.943 2.348-5.292 5.291-5.292z");
+        border.setFill(Paint.valueOf("black"));
+
+        SVGPath stateBorder = new SVGPath();
+        stateBorder.setContent("m15.875 38.612c-3.653 0-6.614 2.962-6.614 6.615v9.178c0 3.653 2.961 6.614 6.614 6.614h126.173c3.653 0 6.615-2.961 6.615-6.614V45.228c0-3.653-2.962-6.615-6.615-6.615zm0 1.323h126.173c2.943 0 5.292 2.349 5.292 5.292v9.178c0 2.943-2.349 5.291-5.292 5.291H15.875c-2.943 0-5.292-2.348-5.292-5.291V45.228c0-2.943 2.349-5.292 5.292-5.292z");
+        stateBorder.setFill(Paint.valueOf("black"));
+
+        SVGPath banner = new SVGPath();
+        banner.setContent("M6.625.665H319.614c3.298 0 5.953 2.655 5.953 5.953v19.103c0 3.298-2.655 5.953-5.953 5.953H6.625c-3.298 0-5.953-2.655-5.953-5.953V6.618c0-3.298 2.655-5.953 5.953-5.953z");
+        banner.setFill(Paint.valueOf("white"));
+
+        SVGPath symbol = new SVGPath();
+        symbol.setScaleX(.5);
+        symbol.setScaleY(.5);
+
+        if(!alpha.isCompetency()){
+            symbol.setContent("m 54.189453,14.023438 c -3.551127,-0.01733 -5.87912,0.05722 -8.908203,0.738281 -3.029104,0.681061 -6.854586,1.927454 -9.574219,4.394531 -2.719637,2.467081 -3.942639,5.851619 -4.658203,8.427734 -0.71556,2.576101 -0.945638,4.687766 -0.628906,8.576172 0.316731,3.888394 1.053524,9.813293 3.939453,13.732422 2.885894,3.919081 7.612935,5.113988 12.492187,5.730469 4.879242,0.616479 10.21018,0.683401 15.671875,0.527344 5.461693,-0.156057 11.715279,-0.655919 16.548829,-6.941407 1.947326,-2.532279 3.623602,-6.040319 5.203125,-9.904297 1.439404,3.884529 2.823201,7.64147 4.412109,11.871094 l 2.476562,-0.929687 c -1.95835,-5.21307 -3.756769,-10.055487 -5.429687,-14.607422 1.763436,-4.848647 3.401317,-10.166807 5.042969,-15.546875 l -2.529297,-0.773438 c -1.305941,4.279865 -2.660786,8.312388 -4.021485,12.275391 -1.80555,-5.009618 -3.153121,-9.031834 -5.888671,-11.861328 -2.817787,-2.914554 -6.81571,-4.20795 -11.236329,-4.900391 -4.42064,-0.692444 -9.360985,-0.791263 -12.912109,-0.808593 z m -0.01172,2.646484 c 3.511426,0.01714 8.355915,0.126077 12.513672,0.777344 4.157779,0.65127 7.532274,1.837173 9.744141,4.125 2.211923,2.287884 3.559593,6.080872 5.458984,11.330078 0.282987,0.782069 0.706237,1.888645 1.011719,2.722656 -1.85966,4.975319 -3.812146,9.214539 -5.931641,11.970703 -4.226856,5.496551 -9.134961,5.756136 -14.52539,5.910156 -5.390427,0.154021 -10.60192,0.08143 -15.265625,-0.507812 -4.663694,-0.589246 -8.480231,-1.668371 -10.69336,-4.673828 -2.213093,-3.00541 -3.130143,-8.653546 -3.433593,-12.378907 -0.30345,-3.725347 -0.119505,-5.267361 0.542968,-7.652343 0.662469,-2.384968 1.780992,-5.269325 3.884766,-7.177735 2.103779,-1.908413 5.583709,-3.143453 8.376953,-3.771484 2.793266,-0.628036 4.804985,-0.690965 8.316406,-0.673828 z");
+        }else {
+            symbol.setContent("m 60.077127,3.356555 -8.843143,17.725284 -19.61502,2.763922 14.125631,13.887857 -3.432832,19.508861 17.573086,-9.142122 17.493467,9.292695 -0.46309,-2.770965 L 74.113388,37.855484 88.357637,24.089493 68.766991,21.157125 Z m -0.02709,6.2655406 6.864581,14.0617184 15.475366,2.316539 -11.251759,10.874244 2.578686,15.433119 -13.818517,-7.34067 -13.881358,7.222052 2.711385,-15.41091 -11.158599,-10.970654 15.494864,-2.183299 z");
+        }
+
+        StackPane pane = new StackPane();
+        pane.setPadding(new Insets(0, 0, 0, 0));
+        pane.maxWidth(300);
+        pane.maxHeight(280);
+        pane.setAlignment(Pos.TOP_LEFT);
+        pane.getChildren().add(back);
+        pane.getChildren().add(banner);
+        pane.getChildren().add(border);
+        pane.getChildren().add(stateBorder);
+        pane.getChildren().add(symbol);
+
+        button.setGraphic(pane);
+
+        tilePane.getChildren().add(button);
+
     }
 
     /**
@@ -549,6 +614,16 @@ public class ui extends Application {
 
         alert.showAndWait();
     }
+
+
+    //Alpha Svg
+    //m 13.236328,0.00195313 c -7.306067,0 -13.2285155,5.92244817 -13.2285155,13.22851587 V 432.05859 c 0,7.30607 5.9224482,13.22852 13.2285155,13.22852 H 639.23047 c 7.30606,0 13.22851,-5.92245 13.22851,-13.22852 V 13.230469 c 0,-7.306068 -5.92245,-13.22851587 -13.22851,-13.22851587 z m 0,2.64453127 H 639.23047 c 5.88604,0 10.58203,4.6979401 10.58203,10.5839846 V 432.05859 c 0,5.88605 -4.69599,10.58399 -10.58203,10.58399 H 13.236328 c -5.8860438,0 -10.5820311,-4.69794 -10.5820311,-10.58399 V 13.230469 c 0,-5.8860449 4.695987,-10.5839846 10.5820311,-10.5839846 z
+    //Alpha stat border
+    //m 31.75,77.224609 c -7.306068,0 -13.228516,5.924401 -13.228516,13.230469 v 18.355472 c 0,7.30606 5.922448,13.22851 13.228516,13.22851 h 252.3457 c 7.30608,0 13.23047,-5.92245 13.23047,-13.22851 V 90.455078 c 0,-7.306067 -5.92439,-13.230469 -13.23047,-13.230469 z m 0,2.646485 h 252.3457 c 5.88606,0 10.58399,4.697938 10.58399,10.583984 v 18.355472 c 0,5.88604 -4.69793,10.58203 -10.58399,10.58203 H 31.75 c -5.886045,0 -10.583984,-4.69599 -10.583984,-10.58203 V 90.455078 c 0,-5.886045 4.697939,-10.583984 10.583984,-10.583984 z
+    //Alpha Banner
+    //M 13.249837,1.3306681 H 639.22835 c 6.59607,0 11.90625,5.3101875 11.90625,11.9062499 v 38.205967 c 0,6.596063 -5.31018,11.90625 -11.90625,11.90625 H 13.249837 c -6.5960622,0 -11.9062497,-5.310187 -11.9062497,-11.90625 V 13.236918 c 0,-6.5960624 5.3101875,-11.9062499 11.9062497,-11.9062499 z
+    //Alpha back
+    //M 12.446792,0.64408761 H 640.02104 c 6.55135,0 11.82554,5.27419489 11.82554,11.82554939 V 432.69513 c 0,6.55135 -5.27419,11.82554 -11.82554,11.82554 H 12.446792 c -6.5513543,0 -11.82554918,-5.27419 -11.82554918,-11.82554 V 12.469637 c 0,-6.5513545 5.27419488,-11.82554939 11.82554918,-11.82554939 z
 
     //SVG for wide card
     // M 13.353516,0.125 C 6.0474485,0.125 0.125,6.0474484 0.125,13.353516 V 432.07031 c 0,7.30607 5.9224482,13.22852 13.228516,13.22852 H 639.24219 c 7.30607,0 13.22851,-5.92245 13.22851,-13.22852 V 13.353516 C 652.4707,6.0474494 646.54826,0.125 639.24219,0.125 Z m 0,2.6445313 H 639.24219 c 5.88605,0 10.58398,4.6979389 10.58398,10.5839847 V 432.07031 c 0,5.88605 -4.69793,10.58399 -10.58398,10.58399 H 13.353516 c -5.8860443,0 -10.5839847,-4.69795 -10.5839847,-10.58399 V 13.353516 c 0,-5.8860448 4.6979401,-10.5839847 10.5839847,-10.5839847 z
