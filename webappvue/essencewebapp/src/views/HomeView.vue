6c9140680e57e5fb7318b95dcbc71dd1435a32f3
sinakhajehpour <sinakhajehpour@isu.edu>
2022-04-24 22:40:39 -0600

    vue application added

--- webappvue/essencewebapp/src/views/HomeView.vue
+++ webappvue/essencewebapp/src/views/HomeView.vue
@@ -1,93 +1,104 @@
 <template>
-    <p>Message is: {{ message }}</p>
-<div class="outer">
-    <div class="top">Alpha card name </div>
-    <input v-model="message" class="alpha" placeholder="Alpha name"/>
-    <input v-model="message" class="moove" placeholder="state1"/>
-    <input v-model="message" class="moove" placeholder="state2"/>
-    <input v-model="message" class="moove" placeholder="state3"/>
-    <input v-model="message" class="moove" placeholder="state4"/>
-    <input v-model="message" class="moove" placeholder="state5"/>
-    <input v-model="message" class="moove" placeholder="state6"/>
-    <input v-model="message" class="brief" placeholder="brief description"/>
-    <input v-model="message" class="detaildescription" placeholder="detail deescription"/>
 
+<body id="body">
+    <h2> Welcome To Essence App</h2>
+    <div class="grid-container">
+        <div id="header" class="header">
+            <h1>
+                Essence Kernel Tools
+            </h1>
+        </div>
 
-   </div>
+        <div id="menu" class="menu">
+            <router-link to="/">Home</router-link> |
+            <router-link to="/about">About</router-link>
+            <router-link to="/practiceCreator">Create Essence Practice</router-link>
+            <router-link to="/cardCatalog">Essence Card Catalog</router-link>
+            <router-link to="/cardCreator">Create Essence Cards</router-link>
+            <p id="menuLabel">Essence games</p>
+            <router-link to="/progressPoker">Progress Poker</router-link>
+            <router-link to="/chaseTheState">Chase The State</router-link>
+            <router-link to="/objectiveGo">Objective Go</router-link>
+            <router-link to="/checkpointConstruction">Checkpoint Construction</router-link>
+            <router-link to="/practiceCatalog">Practice Catalog</router-link>
+            
+            <!--
+            <a href="index.vue">Home</a>
+            <a href="practiceCreator.vue">Create Essence Practice</a>
+            <a href="cardCatalog.vue" class="active">Essence Card Catalog</a>
+            <a href="cardCreator.vue">Create Essence Cards</a>
+            <p id="menuLabel">Essence games</p>
+            <a href="progressPoker.vue">Progress Poker</a>
+            <a href="chaseTheState.vue">Chase the State</a>
+            <a href="objectiveGo.vue">Objective Go</a>
+            <a href="checkpointConstruction.vue">Checkpoint Construction</a>
+            -->
+        </div>
+        <div id="content" class="content">
+
+        </div>
+    </div>
+</body>
 </template>
-
 <style>
-.alpha{
-    display: list-item;
-    background: rgb(225, 225, 68);
-    border-color: rgb(225, 225, 68);
-    position: relative;
-    border-radius: 10px;
-    width: 30%;
-    padding: 10px 20px;
-    top:-60px;
-    left:10%;
-
-     
-
-}
-.moove{
-    display: list-item;
-    background: white;
-    position: relative;
-    border-color: red;
-    border-radius: 10px;
-    width: 30%;
-    padding: 10px 20px;
-    top:0px;
-    left:2%;
-    margin: 20px;
-    bottom: 20px; 
+html{
+    margin: 0px;
+    padding: 0px;
 }
 
-.brief{
-    display: list-item;
-    background: white;
-    position: relative;
-    border-color: red;
-    border-radius: 10px;
-    width: 30%;
-    padding: 50px 20px;
-    top:-355px;
-    left:50%;
-    margin: 20px;
-   
-}
-.detaildescription{
-    display: list-item;
-    background: white;
-    position: relative;
-    border-color: red;
-    border-radius: 10px;
-    width: 30%;
-    padding: 50px 20px;
-    top:-300px;
-    left:50%;
-    margin: 20px;
-    bottom: 70px;
-}
-div.outer{
-    -webkit-border-radius: 20px;
-    -moz-border-radius: 20px;
-    border-radius: 20px;
-    overflow: hidden;
-    border-color: red;
-    background-color: gray;
-    margin: 0px 200px ;
-    padding: 0px 0px;
-    height: 550px;
+#body{
+    margin: 0px;
+    padding: 0px;
+    background-color: #000E0F;
 }
 
-div.outer .top{
-    border-radius: 10px;
-    padding: 30px 240px;
-    width: 50%;
-    background-color: rgb(225, 225, 68);
+.grid-container{
+    display: grid;
+    grid-template-columns: 20% 80%;
+    grid-template-areas:
+    "header content"
+    "menu content"
+    ;
 }
+
+#header{
+    grid-area: header;
+    background-color: #1B5559;
+    color: white;
+    text-align: center;
+    font-size: 20px;
+}
+
+#menu{
+    grid-area: menu;
+    background-color: #1B5559;
+}
+
+#menu a{
+    background-color: #1B5559;
+    color: white;
+    display: block;
+    padding: 12px;
+    text-decoration: none;
+}
+
+#menu a:hover{
+    background-color: #2c7379;
+}
+
+#menu a.active{
+    background-color: #449ca1;
+}
+
+#menuLabel{
+    background-color: #000E0F;
+    color: white;
+    padding: 12px;
+}
+h2{
+    color:#449ca1;
+    font-size: 50px;
+}
+
 
 </style>
